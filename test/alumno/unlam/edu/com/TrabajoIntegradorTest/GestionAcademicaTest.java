23bc0681e10be5005b822193f4297a2b7a9db3a7
Santiago Livieri <santiago.livieri@cauciones.com>
2023-09-18 15:22:39 -0300

    primer commit

---
+++ test/alumno/unlam/edu/com/TrabajoIntegradorTest/GestionAcademicaTest.java
@@ -0,0 +1,14 @@
+package alumno.unlam.edu.com.TrabajoIntegradorTest;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class GestionAcademicaTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
